f255bd352975f04e4fa6028279910555442fd915
Nodenkey <nodenkey@gmail.com>
2020-08-30 14:51:35 +0000

    added responsiveness

--- src/components/about-ai/about-ai.style.js
+++ src/components/about-ai/about-ai.style.js
@@ -3,19 +3,22 @@ import styled from "styled-components";
 
 export const AboutAiWrapper = styled.section`
   text-align: left!important;
-  height: 80vh;
   width: 100vw;
   margin-top: 100px;
   display: grid;
   align-items: center;
-  grid-template-columns: 1fr 1fr;
-  grid-template-areas: 'image text';
-  grid-gap: 100px;
+  grid-template-columns: 1fr;
+  grid-gap: 30px;
+  @media only screen and (min-width: 600px){
+    grid-template-columns: 1fr 1fr;
+    height: 80vh;
+    grid-gap: 100px;
+  }
 `;
 
 
 export const Image = styled.img`
-  grid-area: image;
+  min-height: 300px;
   object-fit: cover;
   object-position: 50% 10%;
   width: 100%;
--- src/components/emergency/emergency.style.js
+++ src/components/emergency/emergency.style.js
@@ -4,9 +4,11 @@ export const EmergencyLayout = styled.div`
   display: grid;
   width: 100%;
   align-items: center;
-  height: 80vh;
-  grid-template-columns: 1fr 1.5fr;
+  grid-template-columns: 1fr;
   grid-gap: 50px;
+  >div{
+  order: 2
+  }
   h1{
   text-align: left;
   line-height: 150%;
@@ -17,10 +19,22 @@ export const EmergencyLayout = styled.div`
   text-align: left;
   }
   img{
+  min-height: 300px;
   object-fit: cover;
+  order: 1;
   object-position: center;
   height: 100%;
   width: 100%;
   filter: grayscale(.5);
+  }
+  @media only screen and (min-width: 600px){
+    grid-template-columns: 1fr 1.5fr;
+    height: 80vh;
+    >div{
+    order: 1
+    }
+    img{
+    order: 2
+    }
   }
 `;
